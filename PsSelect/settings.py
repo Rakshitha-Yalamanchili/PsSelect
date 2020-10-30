93fa5c86d413455f5e2c88f2ff59967a4d6185d6
Rakshitha <Rakshitha@ex.com>
2020-10-30 13:00:35 +0530

    hero django added

--- PsSelect/settings.py
+++ PsSelect/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -70,6 +71,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'PsSelect.wsgi.application'
 
+django_heroku.settings(locals())
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
